b5c0722d5a939b966ad7b56a5d064f2d066bd3ce
DevDoge <lhuthefranc@hotmail.com>
2023-02-02 14:52:32 -0300

    applications, maven dependencies, initial entities and resources and data base H2

---
+++ src/main/java/com/example/springproject/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.example.springproject.resources;
+
+import com.example.springproject.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User user = new User(1L, "Maria", "maria@maria", "999.999", "123456");
+        return ResponseEntity.ok().body(user);
+    }
+
+}
